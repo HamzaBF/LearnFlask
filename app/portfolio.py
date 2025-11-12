f72317eaee48ac6eb375b689c77bebe1dacb5337
bouftama <hamzaaboufatama@gmail.com>
2025-11-12 19:11:51 +0100

    display project data and add project HTML file; make project count dynamic using Jinja2 template

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,4 +1,5 @@
 from flask import Flask , render_template
+from app.modeles import projets
 
 app = Flask(__name__)
 
@@ -6,4 +7,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html',liste=projets)
