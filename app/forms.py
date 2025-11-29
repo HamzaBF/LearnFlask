960c4a0f4463771600ad2d6a2180630053ea04d6
bouftama <hamzaaboufatama@gmail.com>
2025-11-29 19:21:12 +0100

    Enhance FormAvis by adding input validation for 'auteur' and 'contenu' fields

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form , StringField , TextAreaField
+from wtforms import Form , StringField , TextAreaField, validators
 from wtforms.csrf.session import SessionCSRF
 from datetime import timedelta
 
@@ -10,5 +10,10 @@ class FormAvis(Form):
         csrf_secret = b'6QE_LBwhg4JxTi61QmsvpdvgF8'
         csrf_time_limit= timedelta(seconds=10)
         
-    auteur =  StringField('Nom ( entreprise)')
-    contenu = TextAreaField('Votre avis')
+    auteur =  StringField('Nom ( entreprise)',[
+        validators.InputRequired(),
+        validators.Length(min=2,max=50)
+    ])
+    contenu = TextAreaField('Votre avis',[
+        validators.InputRequired()
+    ])
